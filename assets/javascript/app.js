25f43a65cd96e3bd00b2f8597e7c120ea979c97c
Jed garza <Jedgarza@gmail.com>
2019-09-20 16:21:40 -0500

    fixed css

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,9 +12,6 @@ $(document).ready(function () {
 })
 
 
-function ()[
-    
-]
 $('#search-button').on('click', function () {
     // console.log("click");
     var x = $(this).data("search");
@@ -22,17 +19,17 @@ $('#search-button').on('click', function () {
     // console.dir(this);\
     var corsAnywhere = "https://cors-anywhere.herokuapp.com/";
     var queryURL = corsAnywhere + "https://api.yelp.com/v3/businesses/search?q=" + x + "&api_key='bdZhbvY9LhTRAzcG1J4aOAoU8JZBVNmKymfTyFDj44DBUC4fUk5NHiRzkQ6Xcixiujgp0r4wREw8J4666qEw40K3PRQSPu3vQ2LU66okGVMJZ2nAAE-7yHW6du-EXXYx'"
-
     // console.log(queryURL);
-
-    $.ajax({ url: queryURL, method: 'GET' })
+    $.ajax({ url: queryURL, headers:{'Authorization': "Bearer bdZhbvY9LhTRAzcG1J4aOAoU8JZBVNmKymfTyFDj44DBUC4fUk5NHiRzkQ6Xcixiujgp0r4wREw8J4666qEw40K3PRQSPu3vQ2LU66okGVMJZ2nAAE-7yHW6du-EXXYx"
+     }, method: 'GET', dataType: 'json', success: function(data){
+     } })
         .then(function (response) {
-            console.log(response);
+            // console.log(response);
             // for (var i = 0; i < response.data.length; i++) {
-
-
-
             // }
-
         })
-})
+ })
+
+$('#search-button').on('click', function`(){
+    
+    })
