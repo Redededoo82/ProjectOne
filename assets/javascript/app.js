a416b10ed2720f117664d03067bd4cec94e4ebda
AARON M <asmarcks@gmail.com>
2019-09-20 13:54:20 -0500

    search button almost working the way I want it

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,31 +1,34 @@
-$(document).ready(function(){
+$(document).ready(function () {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-          lat = position.coords.latitude;
-          long = position.coords.longitude;
+        navigator.geolocation.getCurrentPosition(function (position) {
+            lat = position.coords.latitude;
+            long = position.coords.longitude;
 
-          console.log(lat)
-          console.log(long)
+            console.log(lat)
+            console.log(long)
 
-        })}
+        })
+    }
 })
 
-$('button').on('click', function () {
+$('#search-button').on('click', function () {
+    // console.log("click");
     var x = $(this).data("search");
     // console.log(x);
-    // console.dir(this);
-    var queryURL = "https://api.yelp.com/v3/businesses/search?q=" + x + "&api_key=bdZhbvY9LhTRAzcG1J4aOAoU8JZBVNmKymfTyFDj44DBUC4fUk5NHiRzkQ6Xcixiujgp0r4wREw8J4666qEw40K3PRQSPu3vQ2LU66okGVMJZ2nAAE-7yHW6du-EXXYx&limit=2"
-    
+    // console.dir(this);\
+    var corsAnywhere = "https://cors-anywhere.herokuapp.com/";
+    var queryURL = corsAnywhere + "https://api.yelp.com/v3/businesses/search?q=" + x + "&api_key='bdZhbvY9LhTRAzcG1J4aOAoU8JZBVNmKymfTyFDj44DBUC4fUk5NHiRzkQ6Xcixiujgp0r4wREw8J4666qEw40K3PRQSPu3vQ2LU66okGVMJZ2nAAE-7yHW6du-EXXYx'"
+
     // console.log(queryURL);
 
     $.ajax({ url: queryURL, method: 'GET' })
-        .done(function (response) {
-            // console.log(response);
-            for (var i = 0; i < response.data.length; i++) {
+        .then(function (response) {
+            console.log(response);
+            // for (var i = 0; i < response.data.length; i++) {
 
-                
 
-            }
+
+            // }
 
         })
 })
