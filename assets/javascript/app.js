c4dc279f50a678954c88dc442698c8065c793089
AARON M <asmarcks@gmail.com>
2019-09-20 11:33:26 -0500

    javascript update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,3 +9,23 @@ $(document).ready(function(){
 
         })}
 })
+
+$('button').on('click', function () {
+    var x = $(this).data("search");
+    // console.log(x);
+    // console.dir(this);
+    var queryURL = "https://api.yelp.com/v3/businesses/search?q=" + x + "&api_key=bdZhbvY9LhTRAzcG1J4aOAoU8JZBVNmKymfTyFDj44DBUC4fUk5NHiRzkQ6Xcixiujgp0r4wREw8J4666qEw40K3PRQSPu3vQ2LU66okGVMJZ2nAAE-7yHW6du-EXXYx&limit=2"
+    
+    // console.log(queryURL);
+
+    $.ajax({ url: queryURL, method: 'GET' })
+        .done(function (response) {
+            // console.log(response);
+            for (var i = 0; i < response.data.length; i++) {
+
+                $('#gifs').prepend("<img src='" + response.data[i].images.downsized.url + "'>");
+
+            }
+
+        })
+})
