ab108dc056ce056acf82258478a8549f31fcd58a
AARON M <asmarcks@gmail.com>
2019-09-22 18:47:18 -0500

    finished debugging. got yelp api to send data to the app. again

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,21 +11,24 @@ $(document).ready(function () {
     }
 })
 
-$('#search-button').on('click', function () {
+$("#search-button").on('click', function () {
     // console.log("click");
     var x = $(".form-control").val();
     // console.log(x);
-    // console.dir(this);\
+    console.dir(this);
     var corsAnywhere = "https://cors-anywhere.herokuapp.com/";
     var queryURL = corsAnywhere + "https://api.yelp.com/v3/businesses/search?location=Austin,tx&q=" + x
 
-    // console.log(queryURL);
+    console.log(queryURL);
 
-    $.ajax({ url: queryURL, headers:{'Authorization': "Bearer bdZhbvY9LhTRAzcG1J4aOAoU8JZBVNmKymfTyFDj44DBUC4fUk5NHiRzkQ6Xcixiujgp0r4wREw8J4666qEw40K3PRQSPu3vQ2LU66okGVMJZ2nAAE-7yHW6du-EXXYx"
-     }, method: 'GET', dataType: 'json', success: function(data){
+    $.ajax({
+        url: queryURL, headers: {
+            'Authorization': "Bearer bdZhbvY9LhTRAzcG1J4aOAoU8JZBVNmKymfTyFDj44DBUC4fUk5NHiRzkQ6Xcixiujgp0r4wREw8J4666qEw40K3PRQSPu3vQ2LU66okGVMJZ2nAAE-7yHW6du-EXXYx"
+        }, method: 'GET', dataType: 'json', success: function (data) {
 
             console.log(data);
-     } })
-        
+        }
+    })
+
 })
 
