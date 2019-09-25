fbab84ecb8b5e03b6853edb3359e2f2bded54ab6
Jed garza <Jedgarza@gmail.com>
2019-09-25 10:19:46 -0500

    Added ifo to append to card

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,7 @@
 
 var userLat;
 var userLong;
-
+var output="";
 $(document).ready(function () {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function (position) {
@@ -28,7 +28,7 @@ $(document).ready(function () {
     $("#search-button").on('click', function () {
 
         var userSearchTerm = $('#userInputField').val().trim();
-        console.log(userSearchTerm);
+        // console.log(userSearchTerm);
 
         var corsAnywhere = "https://cors-anywhere.herokuapp.com/";
         var queryURL = corsAnywhere + `https://api.yelp.com/v3/businesses/search?latitude=${userLat}&longitude=${userLong}&categories=resturants&term=${userSearchTerm}`
@@ -38,8 +38,41 @@ $(document).ready(function () {
                 'Authorization': "Bearer bdZhbvY9LhTRAzcG1J4aOAoU8JZBVNmKymfTyFDj44DBUC4fUk5NHiRzkQ6Xcixiujgp0r4wREw8J4666qEw40K3PRQSPu3vQ2LU66okGVMJZ2nAAE-7yHW6du-EXXYx"
             }, method: 'GET', dataType: 'json', success: function (data) {
                 console.log(data);
-            }
-        });
+            
+        var search =[];
+        for (var i =0; i <data.businesses.length;i++) {
+            search[i] ={};
+            search[i].alias='';
+            console.log(data.businesses[i].name);
+ incoming=data.businesses[i];
+output=search[i];
+
+ if(incoming.name){output.name=incoming.name;}
+ if(incoming.coordinates){
+    if(incoming.coordinates.longitude){output.longitude=incoming.coordinates.longitude;}
+    if(incoming.coordinates.latitude){output.latitude=incoming.coordinates.latitude;}
+ }
+ 
+ 
+        
+        
+
+        }
+        console.log("search = ",search)
+
+
+
+for(var p=0;p<search.length;p++){
+    
+  document.getElementById('plswork').innerHTML +="Name:"+ search[p].name+" Lat:"+search[p].latitude+"  Long: "+search[p].longitude +"<br>"; 
+ 
+}
+
+
+    }
+});
+
+       
     });
 
    
