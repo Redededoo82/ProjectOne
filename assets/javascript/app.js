c1f83d0aebb1f6fa5c0f72a02570c41324ce7743
Rubi Renteria <rubirenteria43@gmail.com>
2019-09-25 20:35:58 -0600

    adding the scroll button and created a table for the yelp api

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,97 +1,71 @@
-
-
 var userLat;
 var userLong;
 var output="";
 $(document).ready(function () {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-            var lat = position.coords.latitude;
-            var long = position.coords.longitude;
-
-            userLat = parseFloat(lat)
-            userLong = parseFloat(long)
-
-
-
-            var mymap = L.map('map').setView([userLat, userLong], 13);
-
-            L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-                attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-                maxZoom: 18,
-                id: 'mapbox.streets',
-                accessToken: 'pk.eyJ1Ijoib2JqZWN0aXZlc2t1bmthbWJhc3NhZG9yIiwiYSI6ImNrMHNrZjg3czAzbWMzbXFzZWltZ2lkeTQifQ.bmyNVE-XAhU1uRbza64fMw'
-            }).addTo(mymap);
-        })
-    }
-<<<<<<< HEAD
-
-=======
->>>>>>> master
-
-    $("#search-button").on('click', function () {
-
-        var userSearchTerm = $('#userInputField').val().trim();
-        // console.log(userSearchTerm);
-
-        var corsAnywhere = "https://cors-anywhere.herokuapp.com/";
-        var queryURL = corsAnywhere + `https://api.yelp.com/v3/businesses/search?latitude=${userLat}&longitude=${userLong}&categories=resturants&term=${userSearchTerm}`
-
-        $.ajax({
-            url: queryURL, headers: {
-                'Authorization': "Bearer bdZhbvY9LhTRAzcG1J4aOAoU8JZBVNmKymfTyFDj44DBUC4fUk5NHiRzkQ6Xcixiujgp0r4wREw8J4666qEw40K3PRQSPu3vQ2LU66okGVMJZ2nAAE-7yHW6du-EXXYx"
-            }, method: 'GET', dataType: 'json', success: function (data) {
-                console.log(data);
-            
-        var search =[];
-        for (var i =0; i <data.businesses.length;i++) {
-            search[i] ={};
-            search[i].alias='';
-            console.log(data.businesses[i].name);
- incoming=data.businesses[i];
+   if (navigator.geolocation) {
+       navigator.geolocation.getCurrentPosition(function (position) {
+           var lat = position.coords.latitude;
+           var long = position.coords.longitude;
+           userLat = parseFloat(lat)
+           userLong = parseFloat(long)
+           var mymap = L.map('map').setView([userLat, userLong], 13);
+           L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+               attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+               maxZoom: 18,
+               id: 'mapbox.streets',
+               accessToken: 'pk.eyJ1Ijoib2JqZWN0aXZlc2t1bmthbWJhc3NhZG9yIiwiYSI6ImNrMHNrZjg3czAzbWMzbXFzZWltZ2lkeTQifQ.bmyNVE-XAhU1uRbza64fMw'
+           }).addTo(mymap);
+       })
+   }
+   $("#search-button").on('click', function () {
+       var userSearchTerm = $('#userInputField').val().trim();
+       // console.log(userSearchTerm);
+       var corsAnywhere = "https://cors-anywhere.herokuapp.com/";
+       var queryURL = corsAnywhere + `https://api.yelp.com/v3/businesses/search?latitude=${userLat}&longitude=${userLong}&categories=resturants&term=${userSearchTerm}`
+       $.ajax({
+           url: queryURL, headers: {
+               'Authorization': "Bearer bdZhbvY9LhTRAzcG1J4aOAoU8JZBVNmKymfTyFDj44DBUC4fUk5NHiRzkQ6Xcixiujgp0r4wREw8J4666qEw40K3PRQSPu3vQ2LU66okGVMJZ2nAAE-7yHW6du-EXXYx"
+           }, method: 'GET', dataType: 'json', success: function (data) {
+               console.log(data);
+       var search =[];
+       for (var i =0; i <data.businesses.length;i++) {
+           search[i] ={};
+        //    search[i].alias='';
+           search[i].image_url='';
+           search[i].rating ='';
+           console.log(data.businesses[i].name);
+incoming=data.businesses[i];
 output=search[i];
+// if(incoming.alias){output.name=incoming.alias;}
+if(incoming.name){output.name=incoming.name;}
+if(incoming.image_url){output.image_url=incoming.image_url;}
+if(incoming.rating){output.rating=incoming.rating;}
 
- if(incoming.name){output.name=incoming.name;}
- if(incoming.coordinates){
-    if(incoming.coordinates.longitude){output.longitude=incoming.coordinates.longitude;}
-    if(incoming.coordinates.latitude){output.latitude=incoming.coordinates.latitude;}
- }
- 
- 
-        
-        
-
-        }
-        console.log("search = ",search)
-
-
-
+       }
+       console.log("search = ",search)
 for(var p=0;p<search.length;p++){
-    
-  document.getElementById('plswork').innerHTML +="Name:"+ search[p].name+" Lat:"+search[p].latitude+"  Long: "+search[p].longitude +"<br>"; 
+//  document.getElementById('plswork').innerHTML +="Name:"+ search[p].name+ "rating"+search[p].rating +"<br>";
+document.getElementById('plswork').innerHTML +=`<tr><th></th><td>${search[p].name}</td></tr>`
+// document.getElementById('plswork').innerHTML +=`<tr2><th></th><td>${search[p].rating}</td></tr2>`
+
  
+// document.getElementById("placeholder").innerHTML+='<img src='+ search[p].image_url+'>';
+$('#placeholder').attr('src', data.businesses[p].image_url);
+
+
 }
 
 
-    }
+   }
 });
-
-       
-    });
-<<<<<<< HEAD
- });
-
- var mymap = L.map('map').setView([userLat, userLong],13);
- console.log(mymap)
- L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-     maxZoom: 18,
-     id: 'mapbox.streets',
-     accessToken: 'pk.eyJ1Ijoib2JqZWN0aXZlc2t1bmthbWJhc3NhZG9yIiwiYSI6ImNrMHNrZjg3czAzbWMzbXFzZWltZ2lkeTQifQ.bmyNVE-XAhU1uRbza64fMw'
- }).addTo(mymap); 
-})
-=======
-
-   
+   });
 });
->>>>>>> master
+var mymap = L.map('map').setView([userLat, userLong],13);
+console.log(mymap)
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1Ijoib2JqZWN0aXZlc2t1bmthbWJhc3NhZG9yIiwiYSI6ImNrMHNrZjg3czAzbWMzbXFzZWltZ2lkeTQifQ.bmyNVE-XAhU1uRbza64fMw'
+}).addTo(mymap);
+
